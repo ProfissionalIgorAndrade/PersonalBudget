1fe12a71d300f5a122ee995b999f98dd36b76e66
Igor Andrade <profissional@igorandrade.com>
2026-01-19 22:48:48 -0300

    chore: Remove Swager package

--- PersonalBudget.Api/Program.cs
+++ PersonalBudget.Api/Program.cs
@@ -6,14 +6,9 @@ using PersonalBudget.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "Personal Budget API", Version = "v1" });
-});
 builder.Services.AddControllers();
 builder.Services.AddScoped<AccountService>();
 builder.Services.AddSingleton<CategoryService>();
-
 
 builder.Services.AddCors(options =>
 {
@@ -28,18 +23,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwagger", false);
-
-if (enableSwagger)
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Personal Budget API v1");
-        c.RoutePrefix = "swagger";
-    });
-}
+// Enable Swagger in all environments
+app.MapOpenApi();
 
 if (!app.Environment.IsDevelopment())
 {
