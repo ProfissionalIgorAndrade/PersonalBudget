38be09bbb4a129dd5fd36bbaa7aff5ec3fb7d038
Igor Andrade <profissional@igorandrade.com>
2026-01-19 22:19:26 -0300

    Add Swagger UI to API

--- PersonalBudget.Api/Program.cs
+++ PersonalBudget.Api/Program.cs
@@ -6,6 +6,10 @@ using PersonalBudget.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Personal Budget API", Version = "v1" });
+});
 builder.Services.AddControllers();
 builder.Services.AddScoped<AccountService>();
 builder.Services.AddSingleton<CategoryService>();
@@ -27,6 +31,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Personal Budget API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 else
 {
